dccab4de7ff1df7ef61427d516310d0fa31d568b
Esra YILDIZ <esrayildiizz@outlook.com>
2022-10-13 17:03:41 +0300

    List gosterildi.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Esra", "Enes", "Nur", "Ayla" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Esra", "Enes", "Nur", "Ayla" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Ayşe"); //Koleksiyonumuzda Ayşe yoktu fakat biz LİST koleksiyonunun içinde hazır gelen Add komutu ile ekleme işlemi yaptık.
+            Console.WriteLine(isimler2[4]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
