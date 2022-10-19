e93accc377b994c7957dc273916869bf44927dd0
Esra YILDIZ <esrayildiizz@outlook.com>
2022-10-19 20:15:00 +0300

    Musteri class'ı olusturuldu.

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; } //bir veri üzerinde matematiksel işlem yapmicaksak string tanımlanabilir.
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string SirketAdi { get; set; }
+        public string TcNo { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
