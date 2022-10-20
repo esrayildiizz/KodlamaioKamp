5f0de505b00e9a2130315c5abbee7d3b59723fdc
Esra YILDIZ <esrayildiizz@outlook.com>
2022-10-20 15:50:59 +0300

    Basvuru yap metodunun icine IKrediManager yazildi cünkü tum kredi tiplerinin referansini tutuyor.

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Önemli
+        public void BasvuruYap(IKrediManager krediManager) //Parantez içine IKrediManager yazdık çünkü tüm kredi tiplerinin referens tutucusudur.
+        {
+            krediManager.Hesapla();
+        }
+    }
+}
