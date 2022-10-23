df0dc6b248076571cdf9f76c20016c17b130e21e
Esra YILDIZ <esrayildiizz@outlook.com>
2022-10-23 15:15:44 +0300

    DatabaseLoggerService aynı zamanda ILoggerService olduğu icin  ondan kalıtım alıyoruz.

---
+++ OOP3/DatabaseLoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    //veritabanına loglama yapıcak süreç..
+    class DatabaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
